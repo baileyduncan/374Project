da1b0aa2aab8f02a779aa748ca3c016be34778e9
jburk02 <164944200+jburk02@users.noreply.github.com>
2024-03-28 15:41:39 -0600

    Update 374_Project.py

--- 374_Project.py
+++ 374_Project.py
@@ -47,11 +47,3 @@ def Hp():
   return head
 
 print('head=', Hp())
-    def resid(fg):
-        return -2*np.log10((2.51/(Re*np.sqrt(fg)))) - (1/np.sqrt(fg))
-    return fsolve(resid, fguess)[0]
-
-#Head loss equation
-#You'll need to set kL equal to an array for this to work
-def Hp(delz, f, L, D, Kl, v, g):
-  return delz + (f*(L/D) + np.sum(kl)v**2/(2*g)
