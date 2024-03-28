1c6cbfb42aa9be8462017da85b861fd25f0adcdc
jburk02 <164944200+jburk02@users.noreply.github.com>
2024-03-28 14:45:59 -0600

    Update 374_Project.py

--- 374_Project.py
+++ 374_Project.py
@@ -1,9 +1,24 @@
 #Code :)
-def func(x):
-  return x + 2
+rho = 62.3 #lbm/ft3
+mu = 6.733E0-4 #lbm/ft*s
+g = 32.174 ft/s^2
+Vdot = 1 #gal/min
 
-James_smells = True
-if James_smells:
-  print("James smells.")
+#Velocity Equation
+def v(Vdot, D):
+  return Vdot*np.pi/4 * D**2
 
-print("Hello World")
+#Reynolds Funciton
+def Re(rho, v, D, mu):
+  return rho * v * D / mu
+
+#Colebrook Equaiton for friction
+def f_cb(esp, D, Re, f, fguess):
+    def resid(fg):
+        return -2*np.log10((2.51/(Re*np.sqrt(fg)))) - (1/np.sqrt(fg))
+    return fsolve(resid, fguess)[0]
+
+#Head loss equation
+#You'll need to set kL equal to an array for this to work
+def Hp(z, f, L, D, Kl, v, g):
+  return z + (f*(L/D) + np.sum(kl)v**2/(2*g)
